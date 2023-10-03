7e9c03c1b5c7fe8703f3cdec7eca3472891c146d
Nikita Timonin <nikita@letsopen.co>
2023-10-03 17:56:52 +0300

    Initial Commit

---
+++ TextBoxTest/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TextBoxTest
+//
+//  Created by Nikita Timonin on 03.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TextBoxTest/TextBoxTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextBoxTestApp.swift
+//  TextBoxTest
+//
+//  Created by Nikita Timonin on 03.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct TextBoxTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
