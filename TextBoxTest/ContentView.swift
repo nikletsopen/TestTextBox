75261c69e780ca8ec1b03ce7cd0e2bc2f8fae107
Nikita Timonin <nikita@letsopen.co>
2023-10-04 19:11:33 +0300

    Initial commit

--- TextBoxTest/ContentView.swift
+++ TextBoxTest/ContentView.swift
@@ -8,12 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var textWidth: CGFloat = 300
+    
+    var string: AttributedString {
+        var string = AttributedString(localized: "Марафон по SwiftUI \"Отцовский пинок\" ")
+        // Add attribute to the whole string
+        string.font = Font.system(size: 14, weight: .medium, design: .rounded)
+        // Add a single attribute to a range
+        if let range = string.range(of: "Марафон") {
+            string[range].foregroundColor = .gray
+            string[range].font = Font.system(size: 14, weight: .light, design: .rounded)
+        }
+        if let range = string.range(of: "\"Отцовский пинок\"") {
+            string[range].foregroundColor = .blue
+            string[range].font = Font.system(size: 34, weight: .heavy, design: .rounded)
+        }
+        
+        return string
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text(string)
+                .frame(width: textWidth, height: 300.0)
+                .border(Color(.red))
+            
+            Slider(value: $textWidth, in: 100...300)
+
         }
         .padding()
     }
